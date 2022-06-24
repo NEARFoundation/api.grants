501afe971b98c3facf2b6758bc70f0665a46a7b2
Sandoche Adittane <sandoche@gmail.com>
2022-06-24 12:13:17 +0200

    Work in progress milestone controller for submission

--- modules/Milestone/MilestoneController.js
+++ modules/Milestone/MilestoneController.js
@@ -1,3 +1,6 @@
+const createSchema = require('./MilestoneFormSchema');
+const loadAndVerifyMilestoneData = require('../../utilities/loadAndVerifyMilestoneData');
+
 /**
  * MilestoneController.js
  *
@@ -6,6 +9,33 @@
 module.exports = {
   async update(req, res) {
     try {
+      const milestone = await loadAndVerifyMilestoneData(req, res);
+
+      // eslint-disable-next-line no-underscore-dangle
+      const milestoneValidationSchema = createSchema(req.__);
+      const result = milestoneValidationSchema.safeParse(req.body.milestoneData);
+
+      const errors = (result && result.error && result.error.issues) || [];
+
+      if (errors.length > 0) {
+        const parsedErrors = {};
+
+        errors.forEach((error) => {
+          const path = error.path.join('.');
+          parsedErrors[path] = error.message;
+        });
+
+        return res.status(400).json({
+          message: 'Invalid grant data',
+          errors: parsedErrors,
+        });
+      }
+
+      milestone.dateSubmission = new Date();
+
+      await milestone.save();
+
+      return res.json(milestone);
     } catch (err) {
       return res.status(500).json({
         message: err.message,
