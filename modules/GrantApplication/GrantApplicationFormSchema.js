4c82491bb642d658cb917708fbe8d7be7fe029ce
Sandoche Adittane <sandoche@protonmail.com>
2022-06-29 09:51:45 +0100

    Fixing missing field in schema

--- modules/GrantApplication/GrantApplicationFormSchema.js
+++ modules/GrantApplication/GrantApplicationFormSchema.js
@@ -45,9 +45,9 @@ const createSchema = (t) =>
         budget: z.number({ invalid_type_error: t('form.budget.error'), required_error: t('form.budget.error') }).min(1, { message: t('form.budget.error') }),
         deliveryDate: z.preprocess(preprocessDate, z.date({ invalid_type_error: t('form.deliveryDate.error'), required_error: t('form.deliveryDate.error') })),
         description: z
-          .string({ required_error: t('form.description.error') })
-          .min(1, { message: t('form.description.error') })
-          .max(100, { message: t('form.description.error') }),
+          .string({ required_error: t('form.milestoneDescription.error') })
+          .min(1, { message: t('form.milestoneDescription.error') })
+          .max(100, { message: t('form.milestoneDescription.error') }),
       }),
     ),
 
