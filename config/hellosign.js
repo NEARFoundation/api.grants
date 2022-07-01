2894b8a52f31f2663b2ef7b94b24c0273874a837
Sandoche Adittane <sandoche@protonmail.com>
2022-07-01 13:45:51 +0100

    Moving to a docx agreement

--- config/hellosign.js
+++ config/hellosign.js
@@ -6,7 +6,7 @@ const config = {
   testMode: parseInt(process.env.HELLO_SIGN_TEST_MODE, 10),
   subject: 'Grant Service Agreement',
   message: 'Please sign the Grant Service Agreement.',
-  templatePath: path.join(__dirname, '../templates/agreement.pdf'),
+  templatePath: path.join(__dirname, '../templates/agreement.docx'),
 };
 
 module.exports = config;
