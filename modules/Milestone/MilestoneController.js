7c7548939bba6aa46eae4c73db2128e71a5090bf
Sandoche Adittane <sandoche@gmail.com>
2022-09-09 14:12:11 +0100

    Adding comment to describe how skipMilestoneInterviewAndApproval work

--- modules/Milestone/MilestoneController.js
+++ modules/Milestone/MilestoneController.js
@@ -191,6 +191,8 @@ module.exports = {
       milestone.proposalNearTransactionHash = proposalNearTransactionHash;
       milestone.isNearProposalValid = true;
 
+      // By setting dateInterviewScheduled, dateInterview & dateValidation,
+      // we are skipping the milestone interview and auto-approve it (only when SKIP_MILESTONE_INTERVIEW_AND_APPROVAL=true)
       if (grantConfig.skipMilestoneInterviewAndApproval) {
         milestone.dateInterviewScheduled = new Date();
         milestone.dateInterview = new Date();
